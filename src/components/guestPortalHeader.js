ad215ae9ec934df652ef0d4a4f60de8b2fc1b118
srikar34 <cs18btech11034@iith.ac.in>
2022-04-29 13:22:16 +0530

    local storage added

--- src/components/guestPortalHeader.js
+++ src/components/guestPortalHeader.js
@@ -1,25 +1,52 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { Navbar,Nav } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
-import { GUEST } from '../assets/guestDetails';
+// import { GUEST } from '../assets/guestDetails';
+// import { useEffect, useRef } from 'react';
 
-class GuestPortalHeader extends Component {
-    render(){
-        return(
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/guest">Hello {GUEST.NAME}</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/guest/services">Services</Nav.Link>
-                        <Nav.Link href="/guest/complaints">Complaints</Nav.Link>
-                        <Nav.Link href="/guest/bill">Bill</Nav.Link>
-                        <Nav.Link href="/guest/rar">Ratings and Reviews</Nav.Link>
-                        <Nav.Link href="/guest/profile">Profile</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        );
+function GuestPortalHeader(){
+
+    const returnName = () => {
+        if(localStorage.getItem('guest')){
+            return JSON.parse(localStorage.getItem('guest')).name;
+        }
+        else{
+            return null;
+        }
     }
+
+    return(
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/guest">Hello {returnName()}</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/guest/services">Services</Nav.Link>
+                    <Nav.Link href="/guest/complaints">Complaints</Nav.Link>
+                    <Nav.Link href="/guest/bill">Bill</Nav.Link>
+                    <Nav.Link href="/guest/rar">Ratings and Reviews</Nav.Link>
+                    <Nav.Link href="/guest/profile">Profile</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 }
+// class GuestPortalHeader extends Component {
+//     render(){
+//         return(
+//             <Navbar bg="dark" variant="dark">
+//                 <Container>
+//                     <Navbar.Brand href="/guest">Hello {GUEST.NAME}</Navbar.Brand>
+//                     <Nav className="me-auto">
+//                         <Nav.Link href="/guest/services">Services</Nav.Link>
+//                         <Nav.Link href="/guest/complaints">Complaints</Nav.Link>
+//                         <Nav.Link href="/guest/bill">Bill</Nav.Link>
+//                         <Nav.Link href="/guest/rar">Ratings and Reviews</Nav.Link>
+//                         <Nav.Link href="/guest/profile">Profile</Nav.Link>
+//                     </Nav>
+//                 </Container>
+//             </Navbar>
+//         );
+//     }
+// }
 
 export default GuestPortalHeader;
