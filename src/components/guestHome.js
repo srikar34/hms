42b95aa8bef6eda4a9a1c5e349747042d38e595c
srikar34 <cs18btech11034@iith.ac.in>
2022-04-29 10:42:41 +0530

    merging with functional code

--- src/components/guestHome.js
+++ src/components/guestHome.js
@@ -84,69 +84,8 @@ function Guest() {
         }
     }
 
-<<<<<<< HEAD
-    render(){
-        return(
-            <div>
-                <GuestPortalHeader />
-                <div >
-                    <h2 className='h1center'> Current Booking Details</h2>
-                    <Form style={{marginLeft:'5%'}}>
-                        <FormGroup  row>
-                            <Label id="roomnumber" md={2}><b>Room No.</b></Label>
-                            <Col md={1}>
-                                <Input disabled value={this.state.room_no}/>
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row >
-                            <Label id="guests" md={2}><b>No. of Guests</b></Label>
-                            <Col md={1}>
-                                <Input disabled value={this.state.num_guests}/>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                </div>
-                <br/>
-                <div style={{marginLeft:'5%'}}>
-                    <Form >
-                        <FormGroup row >
-                            <Label  md={2}><b>Need a Service?</b></Label>
-                            <Col md={2}>
-                                <select value={this.state.selected_service} onChange={this.handleService}>
-                                    <option selected>--select service--</option>
-                                    {Object.values(SERVICES).map(displaydata => (
-                                        <option >{displaydata}</option>
-                                    ))}
-                                </select>
-                            </Col>
-                            <Col>
-                                <Button size='m' variant='primary' disabled={!this.state.selected_service}>Submit</Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                    <Form >
-                        <FormGroup  row>
-                            <Label md={2}><b>Have a Complaint?</b></Label>
-                            <Col md={5} >
-                                <Input rows="2" type="textarea" value={this.state.complaint} placeholder="type your complaint here" onChange={this.handleComplaint}/>
-                            </Col>
-                            <Col>
-                                <Button size='m' variant='primary' disabled={!this.state.complaint}>Submit</Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                    <div className='left'>
-                        <Link to='/guest/contact'>
-                            Contact Helpline?
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        );
-=======
     const handleComplaint = (e) => {
         setComplaint(e.target.value);
->>>>>>> 51ecd4c50bad3f17a0ec0666940a96da5aca1773
     }
 
     // if(snapshot.empty){
